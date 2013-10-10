fef8b904f781b006a1e5878596cb79faf83441a0
renaud-jacques-dagenais <renaud.jacques-dagenais@mail.mcgill.ca>
2013-10-10 16:16:15 -0400

    testGetValue() - Test minimum temperatures
    Added code that checks if the returned minimum temperature values for
    each scale are correct.

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -27,26 +27,34 @@ public class TemperatureTest {
 		/*
 		 * For each temperature scale, this method checks the returned value for:
 		 *     1) A temperature below the scale's minimum
+		 *     2) The minimum temperature
 		 */
 
 		Temperature temperatureBelowMinimum;
+		Temperature minimumTemperature;
 		
-		// Celcius
+		// Celsius
 
 		temperatureBelowMinimum = new Temperature(-274.15, Temperature.Units.CELSIUS);
+		minimumTemperature      = new Temperature(-273.15, Temperature.Units.CELSIUS);
 		
 		assertFalse("Temperature in Celcius cannot be smaller than -273.15", temperatureBelowMinimum.getValue() < -273.15);
+		assertTrue("Minimum Celcius temperature should be -273.15",  minimumTemperature.getValue() == -273.15);
 		
 		// Fahrenheit
 		
 		temperatureBelowMinimum = new Temperature(-460.67, Temperature.Units.FAHRENHEIT);
+		minimumTemperature      = new Temperature(-459.67, Temperature.Units.FAHRENHEIT);
 		
 		assertFalse("Temperature in Fahrenheit cannot be smaller than -459.67", temperatureBelowMinimum.getValue() < -459.67);
+		assertTrue("Minimum Fahrenheit temperature should be -459.67",  minimumTemperature.getValue() == -459.67);
 		
 		// Kelvin
 		
 		temperatureBelowMinimum = new Temperature(-1, Temperature.Units.KELVIN);
+		minimumTemperature      = new Temperature( 0, Temperature.Units.KELVIN);
 		
 		assertFalse("Temperature in Kelvib cannot be smaller than 0", temperatureBelowMinimum.getValue() < 0);
+		assertTrue("Minimum Kelvin temperature should be 0",  minimumTemperature.getValue() == 0);
 	}
 }
