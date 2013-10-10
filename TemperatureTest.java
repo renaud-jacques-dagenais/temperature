2ec711eb8414083f8ae4cfc7c4393a7c7383a700
renaud-jacques-dagenais <renaud.jacques-dagenais@mail.mcgill.ca>
2013-10-10 15:51:14 -0400

    Added testGetValue()
    This method tests the getValue() method of the Temperature class. For
    the moment, it checks the returned values for temperatures below each
    scale's minimum.

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -17,8 +17,36 @@ public class TemperatureTest {
 		Temperature kelvinTemp     = new Temperature(0, Temperature.Units.KELVIN);
 	   
 		// Check if the right units are returned
-		assertSame("Units should be Celcius",    Temperature.Units.CELSIUS,    celciusTemp.getUnits());
+		assertSame("Units should be Celsius",    Temperature.Units.CELSIUS,    celciusTemp.getUnits());
 		assertSame("Units should be Fahrenheit", Temperature.Units.FAHRENHEIT, fahrenheitTemp.getUnits());
 		assertSame("Units should be Kelvin",     Temperature.Units.KELVIN,     kelvinTemp.getUnits());
 	 } 
+	
+	@Test
+	public void testGetValue() {
+		/*
+		 * For each temperature scale, this method checks the returned value for:
+		 *     1) A temperature below the scale's minimum
+		 */
+
+		Temperature temperatureBelowMinimum;
+		
+		// Celcius
+
+		temperatureBelowMinimum = new Temperature(-274.15, Temperature.Units.CELSIUS);
+		
+		assertFalse("Temperature in Celcius cannot be smaller than -273.15", temperatureBelowMinimum.getValue() < -273.15);
+		
+		// Fahrenheit
+		
+		temperatureBelowMinimum = new Temperature(-460.67, Temperature.Units.FAHRENHEIT);
+		
+		assertFalse("Temperature in Fahrenheit cannot be smaller than -459.67", temperatureBelowMinimum.getValue() < -459.67);
+		
+		// Kelvin
+		
+		temperatureBelowMinimum = new Temperature(-1, Temperature.Units.KELVIN);
+		
+		assertFalse("Temperature in Kelvib cannot be smaller than 0", temperatureBelowMinimum.getValue() < 0);
+	}
 }
