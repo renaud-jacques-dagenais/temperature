d4cbb8b2a0d2d4a8834183e300e97de7d16ac009
Renaud Dagenais <renaud.jacques-dagenais@mail.mcgill.ca>
2013-10-10 19:11:56 -0400

    testGetValue() - Test random low temperature
    Added code to check the returned value for a random low temperature (i.e. a temperature within the range [scale minimum, 1000000]).

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -28,33 +28,58 @@ public class TemperatureTest {
 		 * For each temperature scale, this method checks the returned value for:
 		 *     1) A temperature below the scale's minimum
 		 *     2) The minimum temperature
+		 *     3) A random low temperature (between the scale's minimum and 1000000)
 		 */
-
+		
+		double randomLowTemperatureRef, threshold = 1e-6;
 		Temperature temperatureBelowMinimum;
 		Temperature minimumTemperature;
+		Temperature randomLowTemperature;
 		
-		// Celsius
-
+    // Celsius
+		
+		randomLowTemperatureRef  = generateRandomNumber(-273.15, 1000000);
+		
 		temperatureBelowMinimum = new Temperature(-274.15, Temperature.Units.CELSIUS);
 		minimumTemperature      = new Temperature(-273.15, Temperature.Units.CELSIUS);
+		randomLowTemperature    = new Temperature(randomLowTemperatureRef, Temperature.Units.CELSIUS);
 		
 		assertFalse("Temperature in Celcius cannot be smaller than -273.15", temperatureBelowMinimum.getValue() < -273.15);
-		assertTrue("Minimum Celcius temperature should be -273.15",  minimumTemperature.getValue() == -273.15);
+		assertTrue("Minimum Celcius temperature should be -273.15", minimumTemperature.getValue() == -273.15);
+		assertEquals("Celsius random low temperature is incorrect", randomLowTemperatureRef, randomLowTemperature.getValue(), threshold);
 		
 		// Fahrenheit
 		
+		randomLowTemperatureRef  = generateRandomNumber(-459.67, 1000000);
+		
 		temperatureBelowMinimum = new Temperature(-460.67, Temperature.Units.FAHRENHEIT);
 		minimumTemperature      = new Temperature(-459.67, Temperature.Units.FAHRENHEIT);
+		randomLowTemperature    = new Temperature(randomLowTemperatureRef,  Temperature.Units.FAHRENHEIT);
 		
 		assertFalse("Temperature in Fahrenheit cannot be smaller than -459.67", temperatureBelowMinimum.getValue() < -459.67);
-		assertTrue("Minimum Fahrenheit temperature should be -459.67",  minimumTemperature.getValue() == -459.67);
+		assertTrue("Minimum Fahrenheit temperature should be -459.67", minimumTemperature.getValue() == -459.67);
+		assertEquals("Fahrenheit random low temperature is incorrect", randomLowTemperatureRef, randomLowTemperature.getValue(), threshold);
 		
 		// Kelvin
 		
+		randomLowTemperatureRef  = generateRandomNumber(0, 1000000);
+		
 		temperatureBelowMinimum = new Temperature(-1, Temperature.Units.KELVIN);
 		minimumTemperature      = new Temperature( 0, Temperature.Units.KELVIN);
+		randomLowTemperature    = new Temperature(randomLowTemperatureRef, Temperature.Units.KELVIN);
 		
 		assertFalse("Temperature in Kelvib cannot be smaller than 0", temperatureBelowMinimum.getValue() < 0);
-		assertTrue("Minimum Kelvin temperature should be 0",  minimumTemperature.getValue() == 0);
+		assertTrue("Minimum Kelvin temperature should be 0", minimumTemperature.getValue() == 0);
+		assertEquals("Kelvin random low temperature is incorrect", randomLowTemperatureRef, randomLowTemperature.getValue(), threshold);
+	}
+	
+	/**
+	 * Generate a random number between [min, max]
+	 * @param min
+	 * @param max
+	 * @return
+	 */
+	private double generateRandomNumber(double min, double max) {
+		return (min + Math.random() * ((max - min) + 1));
 	}
 }
